5458d749d4f9370bcf7971d9233967b3ea871edb
maxkaft <37123008+maxkaft@users.noreply.github.com>
2018-03-07 14:37:15 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Split_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<List<string>> numbers = Console.ReadLine()
+                .Split('|').Reverse().
+                Select(s => s.Split(new char[] { ' ' },
+                StringSplitOptions.RemoveEmptyEntries).ToList()).ToList();
+                                
+
+            
+
+            Console.WriteLine(string.Join(" ", number));
+            
+                           
+        }
+    }
+}
